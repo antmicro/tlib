814190c28ed5c04b909f7bd15c0c6beffc5c2aa2
Martin Jonsson <mjonsson@internships.antmicro.com>
2025-03-14 07:52:14 +0100

    [#72187] tcg: Document address translation functions

--- include/address-translation.h
+++ include/address-translation.h
@@ -1,4 +1,27 @@
 #pragma once
 
+/*
+ *  Translates a page-aligned guest address into its corresponding host address,
+ *  filling the translation lookaside buffer (TLB) if it wasn't already cached.
+ *  The access width is 32.
+ *
+ *  The address must not span pages, as the pages are not necessarily contiguous
+ *  in memory and parts of the value may therefore lie at completely different
+ *  host addresses.
+ *
+ *  In case the guest address corresponds to an MMIO address, it is returned unchanged.
+ */
 uintptr_t translate_page_aligned_address_and_fill_tlb_u32(target_ulong addr, uint32_t mmu_idx);
+
+/*
+ *  Translates a page-aligned guest address into its corresponding host address,
+ *  filling the translation lookaside buffer (TLB) if it wasn't already cached.
+ *  The access width is 64.
+ *
+ *  The address must not span pages, as the pages are not necessarily contiguous
+ *  in memory and parts of the value may therefore lie at completely different
+ *  host addresses.
+ *
+ *  In case the guest address corresponds to an MMIO address, it is returned unchanged.
+ */
 uintptr_t translate_page_aligned_address_and_fill_tlb_u64(target_ulong addr, uint32_t mmu_idx);
