b682ca48bccddd88d97f2238fa01c126d09811cb
Marek Slowinski <mslowinski@antmicro.com>
2025-06-17 08:44:56 +0200

    [#77443] arm_common: Set logs related to sysregs to DEBUG level

--- arch/arm_common/helper_common.c
+++ arch/arm_common/helper_common.c
@@ -10,12 +10,12 @@
 
 void HELPER(warn_cp_invalid_el)(CPUState *env, void *reg_info, uint32_t current_el)
 {
-    tlib_printf(LOG_LEVEL_WARNING, "[PC=0x%" PC_FMT "] The '%s' register shouldn't be accessed on EL%d", CPU_PC(env),
+    tlib_printf(LOG_LEVEL_DEBUG, "[PC=0x%" PC_FMT "] The '%s' register shouldn't be accessed on EL%d", CPU_PC(env),
                 ((ARMCPRegInfo *)reg_info)->name, current_el);
 }
 
 void HELPER(warn_cp_invalid_access)(CPUState *env, void *reg_info, uint32_t isread)
 {
-    tlib_printf(LOG_LEVEL_WARNING, "[PC=0x%" PC_FMT "] The '%s' register shouldn't be %s", CPU_PC(env),
+    tlib_printf(LOG_LEVEL_DEBUG, "[PC=0x%" PC_FMT "] The '%s' register shouldn't be %s", CPU_PC(env),
                 ((ARMCPRegInfo *)reg_info)->name, isread ? "read from" : "written to");
 }
