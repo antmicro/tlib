17ba9e47c42ddb492c52e37686d27a39c55a4ed0
Jan Malek <jmalek@antmicro.com>
2025-10-07 11:34:38 +0200

    [DEV] atomics: Improve abort message

--- atomic.c
+++ atomic.c
@@ -19,7 +19,8 @@ static inline void ensure_locked_by_me(struct CPUState *env)
     }
 
     if(env->atomic_memory_state->locking_cpu_id != env->atomic_id) {
-        tlib_abort("Tried to release global memory lock by the cpu that does not own it!");
+        tlib_abortf("Tried to release global memory lock by the cpu that does not own it!; locking CPU: %u, releasing CPU: %u",
+                    env->atomic_memory_state->locking_cpu_id, env->atomic_id);
     }
 #endif
 }
