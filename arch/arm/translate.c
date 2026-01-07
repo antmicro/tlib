ab9cfc1bb686bb103af38814d23f0b34330fd086
Jan Wolski <jwolski@antmicro.com>
2026-01-07 14:43:25 +0100

    [#89456] arm: Add FPv5 VRINTR, VRINTZ, VRINTX

--- arch/arm/translate.c
+++ arch/arm/translate.c
@@ -3763,6 +3763,43 @@ static int disas_vfp_insn(CPUState *env, DisasContext *s, uint32_t insn)
                                     gen_vfp_F1_ld0(dp);
                                     gen_vfp_cmpe(dp);
                                     break;
+                                case 12: /* vrintr */
+                                {
+                                    TCGv_ptr fpst = get_fpstatus_ptr(0);
+                                    if(dp) {
+                                        gen_helper_rintd(cpu_F0d, cpu_F0d, fpst);
+                                    } else {
+                                        gen_helper_rints(cpu_F0s, cpu_F0s, fpst);
+                                    }
+                                    tcg_temp_free_ptr(fpst);
+                                    break;
+                                }
+                                case 13: /* vrintz */
+                                {
+                                    TCGv_ptr fpst = get_fpstatus_ptr(0);
+                                    TCGv_i32 tcg_rmode = tcg_const_i32(float_round_to_zero);
+                                    gen_helper_set_rmode(tcg_rmode, tcg_rmode, fpst);
+                                    if(dp) {
+                                        gen_helper_rintd(cpu_F0d, cpu_F0d, fpst);
+                                    } else {
+                                        gen_helper_rints(cpu_F0s, cpu_F0s, fpst);
+                                    }
+                                    gen_helper_set_rmode(tcg_rmode, tcg_rmode, fpst);
+                                    tcg_temp_free_i32(tcg_rmode);
+                                    tcg_temp_free_ptr(fpst);
+                                    break;
+                                }
+                                case 14: /* vrintx */
+                                {
+                                    TCGv_ptr fpst = get_fpstatus_ptr(0);
+                                    if(dp) {
+                                        gen_helper_rintd_exact(cpu_F0d, cpu_F0d, fpst);
+                                    } else {
+                                        gen_helper_rints_exact(cpu_F0s, cpu_F0s, fpst);
+                                    }
+                                    tcg_temp_free_ptr(fpst);
+                                    break;
+                                }
                                 case 15: /* single<->double conversion */
                                     if(dp) {
                                         gen_helper_vfp_fcvtsd(cpu_F0s, cpu_F0d, cpu_env);
