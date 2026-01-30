adadffae85cc647033932e8a3b9886f7cf34eca6
Marek Slowinski <mslowinski@antmicro.com>
2026-01-30 12:47:46 +0100

    [#88150] riscv: fpu: softfloat3: fcvt.s.hw

--- arch/riscv/fpu_helper.c
+++ arch/riscv/fpu_helper.c
@@ -592,9 +592,13 @@ uint64_t helper_fcvt_lu_s(CPUState *env, uint64_t frs1, uint64_t rm)
 uint64_t helper_fcvt_s_hw(CPUState *env, target_ulong rs1, uint64_t rm)
 {
     require_fp;
-    set_float_rounding_mode(RM, &env->fp_status);
-    rs1 = int32_to_float32((int16_t)rs1, &env->fp_status);
-    set_fp_exceptions();
+    set_float3_rounding_mode(RM_3);
+
+    float16_t f1;
+    f1.v = (int16_t)rs1;
+    rs1 = f16_to_f32(f1).v;
+
+    set_fp3_exceptions();
     mark_fs_dirty();
     return rs1;
 }
