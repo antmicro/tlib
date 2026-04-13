bd7b3be731dd08f2eda45835231fc4a1581b3775
Bartlomiej Stefanski <bstefanski@internships.antmicro.com>
2026-04-13 17:00:07 +0200

    [#88885] cores.sh: List core targets

---
+++ cores.sh
@@ -0,0 +1,6 @@
+# This list contains all core targets.
+# If you are adding a new core or endianness add it here.
+# 
+# In the list below the architecture name is followed by "64" sufix for 64 bit cores.
+# All cores include ".le"/".be" sufix determining little/big endianness.
+CORES=(arm.le arm.be arm64.le arm-m.le arm-m.be ppc.le ppc.be ppc64.le ppc64.be i386.le x86_64.le riscv.le riscv64.le sparc.le sparc.be xtensa.le)
